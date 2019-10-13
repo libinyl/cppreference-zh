4e4ce6036448c5f5d07f4d35716355a275ae3939
libinyl <libin152@outlook.com>
2019-10-13 11:37:09 +0800

    add source

---
+++ common/site_scripts.js
@@ -0,0 +1,4 @@
+if(window.mw)
+mw.loader.state({"site":"ready"});
+/* cache key: mwiki1-mwiki_zh_:resourceloader:filter:minify-js:7:bd6e88e91c5e8b9c8e1a906e6fa7f64d */
+/* Cached 20190825000846 */
---
+++ common/skin_scripts.js
@@ -0,0 +1,4 @@
+if(window.mw)
+mw.loader.state({"skins.cppreference2":"ready"});
+/* cache key: mwiki1-mwiki_zh_:resourceloader:filter:minify-js:7:728f164a1dd8ee369e4d25e8cde66537 */
+/* Cached 20190825000846 */
